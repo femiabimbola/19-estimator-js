2564bd7e96321fe937e7197f7e333736a0d24fd3
Djtush <femiabimbola6@gmail.com>
2020-04-13 20:46:10 +0100

    frontend-fix

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+import "./App.css";
+
+const App = () => (
+  <div className="app">
+    <div className="container">
+      <h1>Covid-19 Estimator</h1>
+      <form>
+        <div className="form-group">
+          <label htmlFor="population">Population</label>
+          <input
+            required
+            type="number"
+            id="population"
+            data-population
+            placeholder="Enter total population"
+            name="population"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="period-type">Pick Period Type</label>
+          <select
+            name="period-type"
+            id="period-type"
+            data-period-type='["", "days", "weeks", "months"]'
+          >
+            <option data-period-type value="days">
+              Days
+            </option>
+            <option data-period-typevalue="weeks">Weeks</option>
+            <option data-period-type value="months">
+              Months
+            </option>
+          </select>
+        </div>
+        <div className="form-group">
+          <label htmlFor="timeToElapse">Time To Elapse </label>
+          <input
+            required
+            type="number"
+            id="timeToElapse"
+            data-reported-cases
+            placeholder="Enter time to elapse"
+            name="timeToElapse"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="reported-cases">Enter Reported Cases </label>
+          <input
+            required
+            type="number"
+            id="reported-cases"
+            data-time-to-elapse
+            placeholder="Enter number of reported cases"
+            name="reportedCases"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="total-hospital-bed">Total Hospital Beds</label>
+          <input
+            required
+            type="number"
+            id="total-hospital-bed"
+            data-total-hospital-beds
+            placeholder="Enter total number of beds"
+            name="totalHospitalBed"
+          />
+        </div>
+        <br />
+        <button
+          type="submit"
+          className="button"
+          data-go-estimate
+          htmlFor="data-go-estimate"
+          name="data-go-estimate"
         >
-          Learn React
-        </a>
-      </header>
+          Get Impact and Severe Impact Estimate
+        </button>
+      </form>
     </div>
-  );
-}
+  </div>
+);
 
 export default App;
